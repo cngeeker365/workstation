91c2682649cfee785d782234b5397ed3bc91c332
cngeeker365 <277263044@qq.com>
2019-08-28 08:08:14 +0800

    julia learning

---
+++ go/src/go-learn/LinkedList.go
@@ -0,0 +1,12 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-08-26 23:39:36
+ * @LastEditTime: 2019-08-26 23:45:25
+ * @LastEditors: Please set LastEditors
+ */
+package main
+import "fmt"
+func main(){
+	fmt.Println("hahaha")
+}
