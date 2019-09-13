5007823252868cd484473693ae34c419a98050a6
cngeeker365 <277263044@qq.com>
2019-09-13 15:47:53 +0800

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+<!--
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-08-26 20:50:40
+ * @LastEditTime: 2019-08-26 20:50:40
+ * @LastEditors: your name
+ -->
--- go/src/go-learn/LinkedList.go
+++ go/src/go-learn/LinkedList.go
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-08-26 23:39:36
- * @LastEditTime: 2019-08-26 23:45:25
+ * @LastEditTime: 2019-08-31 00:31:41
  * @LastEditors: Please set LastEditors
  */
 package main
