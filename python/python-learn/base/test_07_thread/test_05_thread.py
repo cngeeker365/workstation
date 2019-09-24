9185f2f7d30781898240f3d93f66a4543ab32f75
Chao_liang <chao.liang@sminedata.com>
2019-09-24 20:58:04 +0800

    python thread

---
+++ python/python-learn/base/test_07_thread/test_05_thread.py
@@ -0,0 +1,37 @@
+'''
+Python的标准库提供了两个模块：
+    _thread是低级模块
+    threading是高级模块，对_thread进行了封装。绝大多数情况下，我们只需要使用threading这个高级模块。
+
+Python的线程虽然是真正的线程，但解释器执行代码时，有一个GIL锁：Global Interpreter Lock，任何Python线程执行前，必须先获得GIL锁，
+然后，每执行100条字节码，解释器就自动释放GIL锁，让别的线程有机会执行。这个GIL全局锁实际上把所有线程的执行代码都给上了锁，所以，
+多线程在Python中只能交替执行，即使100个线程跑在100核CPU上，也只能用到1个核。
+
+在Python中，可以使用多线程，但不要指望能有效利用多核。如果一定要通过多线程利用多核，那只能通过C扩展来实现，不过这样就失去了Python简单易用的特点。
+
+Python虽然不能利用多线程实现多核任务，但可以通过多进程实现多核任务。多个Python进程有各自独立的GIL锁，互不影响。
+'''
+
+# 启动一个线程就是把一个函数传入并创建Thread实例，然后调用start()开始执行
+
+import time
+import threading
+
+# 新线程执行的代码:
+
+
+def loop():
+    print('thread %s is running...' % threading.current_thread().name)
+    n = 0
+    while n < 5:
+        n = n + 1
+        print('thread %s >>> %s' % (threading.current_thread().name, n))
+        time.sleep(1)
+    print('thread %s ended.' % threading.current_thread().name)
+
+
+print('thread %s is running...' % threading.current_thread().name)
+t = threading.Thread(target=loop, name='LoopThread')
+t.start()
+t.join()
+print('thread %s ended.' % threading.current_thread().name)
