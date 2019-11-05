0aa295f75c3d6942cf0622d345d8cc2599f1159c
taobaibai <277263044@qq.com>
2019-11-05 21:46:36 +0800

    update

--- python/python-learn/base/test.py
+++ python/python-learn/base/test.py
@@ -76,3 +76,7 @@
 #         words = contents.split()
 #         num_words = len(words)
 #         print("The file " + filename + " has about " + str(num_words) + " words.")
+
+
+s = "123456789"
+print(s[-1:])
